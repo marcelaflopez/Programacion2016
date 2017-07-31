ab016ba94b7930a52adb183c05669a7b2a344f2f
Fernando <fernandoexequielorquera@gmail.com>
2017-07-31 19:46:16 -0300

    Add files via upload

--- Practico 2/Ejercicio3.c
+++ Practico 2/Ejercicio3.c
@@ -8,7 +8,7 @@ Año: 2017
 typedef int listaInt[50];
 
 void cargarLista(listaInt lista, int *tam);
-int cantMultiplos(listaInt lista, int tam, int A, int B);
+int cantDivisores(listaInt lista, int tam, int A, int B);
 
 int main() {
 	listaInt lista;
@@ -19,8 +19,8 @@ int main() {
 	scanf("%d", &A);
 	printf("Ingrese el valor para B:\n");
 	scanf("%d", &B);	
-	printf("La cantidad de elementos que son solo multpilos de A: %d\n", cantMultiplos(lista, tam, A, B));
-	printf("La cantidad de elementos que son solo multpilos de B: %d\n", cantMultiplos(lista, tam, B, A));
+	printf("La cantidad de elementos que son solo divisores de A: %d\n", cantDivisores(lista, tam, A, B));
+	printf("La cantidad de elementos que son solo divisores de B: %d\n", cantDivisores(lista, tam, B, A));
 	return 0;
 }
 
@@ -36,20 +36,19 @@ void cargarLista(listaInt lista, int *tam){
 }
 
 /*
-Retorna la cantidad elementos de la lista que son multiplos de A
+Retorna la cantidad elementos de la lista que son divisores de A
 pero no de B.
 Ejemplo:
- cantMultiplos([1,10,15,12,6,9,20], 5, 3) -> 2
- cantMultiplos([1,10,15,12,6,9,20], 3, 5) -> 3
- cantMultiplos([1,10,15,12,6,9,20], 7, 5) -> 0
+ cantDivisores([1,10,15,12,6,9,3], 7, 15, 10) -> 2
+ cantDivisores([1,10,15,12,6,9,3], 7, 10, 15) -> 1
 */
-int cantMultiplos(listaInt lista, int tam, int A, int B){
+int cantDivisores(listaInt lista, int tam, int A, int B){
 	int cantidad;
 	int i;
 	
 	cantidad = 0;
 	for(i = 1; i <= tam; i++){
-		if(lista[i] % A == 0 && lista[i] % B != 0){
+		if(A % lista[i] == 0 && B % lista[i] != 0){
 			cantidad = cantidad + 1;
 		}
 	}

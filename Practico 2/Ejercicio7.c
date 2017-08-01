b9627f8eebbcc0e685858f364ced21adb61bde92
Fernando <fernandoexequielorquera@gmail.com>
2017-08-01 19:25:00 -0300

    Add files via upload

--- Practico 2/Ejercicio7.c
+++ Practico 2/Ejercicio7.c
@@ -2,80 +2,42 @@
 #include <ctype.h>
 /*
 Autor: Fernando Orquera
-AÃ±o: 2017 
+Año: 2017 
 */
 
 typedef char listaChar[50];
 
-void cargarLista(listaChar lista, int *tam);
-void trimLista(listaChar lista, int *tam);
-void eliminarCaracter(listaChar lista, int tam, int posi);
+void cargarTexto(listaChar lista, int *tam);
 void enMinusculas(listaChar lista, int tam);
-int cantidadPalabras(listaChar lista, int tam);
-int cantidadVocales(listaChar lista, int tam);
-int cantidadConsonantes(listaChar lista, int tam);
+int esIsograma(listaChar lista, int tam);
+
 
 int main() {
-	int tam;
+	int tam, rpta;
 	listaChar lista;	
 	
-	cargarLista(lista, &tam);
-	trimLista(lista, &tam);
+	cargarTexto(lista, &tam);	
 	enMinusculas(lista, tam);
-	printf("Cantidad de palabras:%d\n", cantidadPalabras(lista, tam));
-	printf("Cantidad de vocales:%d\n", cantidadVocales(lista, tam));
-	printf("Cantidad de consonantes:%d\n", cantidadConsonantes(lista, tam));
+	rpta = esIsograma(lista, tam);
+	if(rpta == 1){
+		printf("El texto es un isograma.\n");
+	}else{
+		printf("El texto no es un isograma.\n");
+	}
 	
 	return 0;
 }
 
 
-void cargarLista(listaChar lista, int *tam){
+void cargarTexto(listaChar lista, int *tam){
 	int i;
 	
-	printf("Ingrese el tama%co de la lista:\n", 164); /*Estoy agrendo el caracter Ã±*/
+	printf("Ingrese el tama%co de la lista:\n", 164); /*Estoy agrendo el caracter ñ*/
 	scanf("%d", tam);
 	for(i = 1; i <= *tam; i++){
-		printf("Ingrese %d %c caracter:", i, 167); /*Estoy agregando el caracter Âº*/
+		printf("Ingrese %d %c caracter:", i, 167); /*Estoy agregando el caracter º*/
 		fflush(stdin);
 		scanf("%c", &lista[i]);
-	}
-}
-
-/*
-	Borra los espaciones en blanco de adelante y de atras de la lista.
-	Ejemplos:
-		trimLista([" ", " ", "a", "b", " ","c", " "], 7) -> (["a", "b", " ","c"], 4)
-		trimLista(["a", "b",  " ","c"], 4) -> (["a", "b", " ","c"], 4) 
-*/
-void trimLista(listaChar lista, int *tam){
-	int i;
-	
-	i = 1;
-	while(lista[i] == ' ' && i <= *tam){
-		eliminarCaracter(lista, *tam, i);
-		*tam = *tam - 1;		
-	}
-	i = *tam;
-	while(lista[i] == ' ' && i >= 1){
-		eliminarCaracter(lista, *tam, i);
-		*tam = *tam - 1;
-		i--;
-	}
-}
-
-/*
-	Elimina un caracter de la lista.
-	No reduce el tamaÃ±o.
-	Ejemplos:
-		eliminarCaracter(["a", "b", "c"], 3, 1) -> (["b", "c", "c"])
-		eliminarCaracter(["a", "b", "c"], 3, 2) -> (["a", "c", "c"])
-*/
-void eliminarCaracter(listaChar lista, int tam, int posi){
-	int i;
-	
-	for(i = posi; i <= tam - 1; i++){
-		lista[i] = lista[i + 1];
 	}
 }
 
@@ -94,52 +56,30 @@ void enMinusculas(listaChar lista, int tam){
 }
 
 /*
-	Devuelve la cantidad de palabras, si se ingresaron solo espacios en blanco devuelve 0.
-	Espera una lista sin espacios en blanco adelante o al final.
+	Devuelve 1 si el texto  es un isograma, -1 en caso contrario.
+	No controla si solo se ingresaron espacios.
 	Ejemplos:
-		cantidadPalabras(["a", "b", " ","c"], 4) -> 2
-		cantidadPalabras(["a", "b", "c"], 3) -> 1
+		cantidadPalabras(["b", "a", " ","c", "k", "g", "r","o", "u", "n", "d"], 11) -> 1
+	    cantidadPalabras(["i", "s", "o", " ","g", "r", "a", "m", "a"], 9) -> -1
+		cantidadPalabras([" ", " ", " "], 3) -> 1
 */
-int cantidadPalabras(listaChar lista, int tam){
-	int i, cantidad;
+int esIsograma(listaChar lista, int tam){
+	int i, j, respuesta;
 	
-	if(tam > 0){
-		cantidad = 1;
-		for(i = 1; i <= tam; i++){
-			if(lista[i] == ' '){
-				cantidad++;
+	respuesta = 1;
+	i = 1;
+	while(i < tam && respuesta == 1){
+		j = i + 1;
+		while(j <= tam && respuesta == 1){
+			if(lista[i] == lista[j] && lista[i] != ' '){
+				respuesta = -1;
 			}
+			j++;
 		}
-	}else{
-		cantidad = 0;
-	}
+		i++;
+	}	
 	
-	return cantidad;
+	return respuesta;
 }
 
-int cantidadVocales(listaChar lista, int tam){
-	int i,cantidad;
-	
-	cantidad = 0;
-	for(i = 1; i <= tam; i++){
-		if(lista[i] == 'a' || lista[i] == 'e' || lista[i] == 'i' || lista[i] == 'o' || lista[i] == 'u'){
-			cantidad++;
-		}
-	}
-	
-	return cantidad;
-}
-
-int cantidadConsonantes(listaChar lista, int tam){
-	int i,cantidad;
-	
-	cantidad = 0;
-	for(i = 1; i <= tam; i++){
-		if(lista[i] != 'a' && lista[i] != 'e' && lista[i] != 'i' && lista[i] != 'o' && lista[i] != 'u' && lista[i] != ' '){
-			cantidad++;
-		}
-	}
-	
-	return cantidad;
-}
 

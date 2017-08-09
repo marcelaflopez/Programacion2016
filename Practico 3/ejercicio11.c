e6de78eb553390be247588ef1c2ae21cf0a111fc
Fernando <fernandoexequielorquera@gmail.com>
2017-08-09 19:27:41 -0300

    Add files via upload

--- Practico 3/ejercicio11.c
+++ Practico 3/ejercicio11.c
@@ -7,88 +7,64 @@ Autor: Fernando Orquera
 Año: 2017
 */
 
-/*Modulos para generar la fecha al azar*/
-void generarFecha(int *anio, int *mes, int *dia);
-int generarAnio();
-int generarMes();
-int generarDia(int mes);
+/*Modulos para generar la hora al azar*/
+int generarHora();
+int generarMinutos();
+int generarSegundos();
 int generarRand(int A, int B);
 
-/*Modulos para calcular cantidad de dias dada una fecha*/
-int diasMes(int mes);
-unsigned long calcularDias(int dia, int mes, int anio);
+/*Modulos para calcular cantidad de segundos dada una hora*/
+unsigned long calcularSegundos(int hora, int minutos, int segundos);
 
 /*Modulos para ingresar una fecha*/
-void ingresarFecha(int *anio, int *mes, int *dia);
-int ingresarAnio();
-int ingresarMes();
-int ingresarDia(int mes);
+int ingresarHora();
+int ingresarSegundos();
+int ingresarMinutos();
 int ingresar(int A, int B);
 
 
 int main() {
-	int anioAzar, mesAzar, diaAzar, anio, mes, dia, tam, i;	
-	double cantidadDias;
+	int horaAzar, minutosAzar, segundosAzar, hora, minutos, segundos, tam, i;	
+	double cantidadSegundos;
 	
 	srand(time(NULL));
 	
-	cantidadDias = 0;
-	generarFecha(&anioAzar, &mesAzar, &diaAzar);
-	printf("Fecha generada al azar %d/%d/%d .\n", diaAzar, mesAzar, anioAzar);
-	printf("Ingrese cantidad de fechas:\n");
+	cantidadSegundos = 0;
+	horaAzar = generarHora();
+	minutosAzar = generarMinutos();
+	segundosAzar = generarSegundos();
+	printf("Hora generada al azar %d-%d-%d .\n", horaAzar, minutosAzar, segundosAzar);
+	printf("Ingrese cantidad de horas:\n");
 	scanf("%d", &tam);
 	for(i = 1; i <= tam; i++){
-		ingresarFecha(&anio, &mes, &dia);
-		cantidadDias = cantidadDias + abs(calcularDias(dia, mes, anio) - calcularDias(diaAzar, mesAzar, anioAzar));		
+		hora = ingresarHora();
+		minutos = ingresarMinutos();
+		segundos = ingresarSegundos();
+		cantidadSegundos = cantidadSegundos + abs(calcularSegundos(hora, minutos, segundos) - calcularSegundos(horaAzar, minutosAzar, segundosAzar));		
 	}
-	printf("El promedio es: %2.f dias", cantidadDias / tam);
+	printf("\nEl promedio es: %2.f segundos", cantidadSegundos / tam);
 		
 	return 0;
 }
 
 /*
-	Genera una fecha valida.
-	Formato dd/mm/aaaa.
+	Genera una hora entre el rango [0, 23]
 */
-void generarFecha(int *anio, int *mes, int *dia){
-	*anio = generarAnio();
-	*mes = generarMes();
-	*dia = generarDia(*mes);
+int generarHora(){
+	return generarRand(0, 23);
+}
+/*
+	Genera minutos en el rango [0, 59].
+*/
+int generarMinutos(){	
+	return generarRand(0, 59);
 }
 
 /*
-	Genera un anio entre el rango [1970, 2038]
+	Genera segundos en el rango [0, 59].
 */
-int generarAnio(){
-	return generarRand(1970, 2038);
-}
-/*
-	Genera un mes.
-*/
-int generarMes(){	
-	return generarRand(1, 12);
-}
-
-/*
-	Genera un dia de acuerdo al mes.
-*/
-int generarDia(int mes){
-	int dia;
-	
-	switch(mes){
-		case 1:case 3:case 5: case 7:
-		case 8:case 10:case 12:
-			dia = generarRand(1, 31);
-			break;
-		case 4:case 6:case 9:case 11:	
-			dia = generarRand(1, 30);
-			break;
-		case 2:
-			dia = generarRand(1, 28);
-			break;
-	}
-	
-	return dia;
+int generarSegundos(){
+	return generarRand(0, 59);
 }
 
 /*
@@ -98,109 +74,62 @@ int generarRand(int A, int B){
 	return (rand() * (B - A) / RAND_MAX) + A;
 }
 
+
 /*
-	Retorna la cantidad de dias que tiene un mes.
-	Ejemplos;
-		diasMes(12) -> 31
-		diasMes(2) -> 28
-*/
-int diasMes(int mes){
-	int dias;
-	
-	switch(mes){
-		case 1:case 3:case 5: case 7:
-		case 8:case 10:case 12:
-			dias = 31;
-			break;
-		case 4:case 6:case 9:case 11:	
-			dias = 30;
-			break;
-		case 2:
-			dias = 28;
-			break;
-	}
-	
-	return dias;
-}
-/*
-	Calcula la cantidad de dias dada una fecha.
+	Calcula la cantidad de segundos dada una hora.
 	Ejemplos:
-		calcularDias(1, 1, 2017) -> 736206
-		calcularDias(1, 1, 2000) -> 730001
+		calcularDias(23, 12, 59) -> 83579
+		calcularDias(7, 55, 15) -> 28515
 */
-unsigned long calcularDias(int dia, int mes, int anio){
-	unsigned long dias;
-	int i;
+unsigned long calcularSegundos(int hora, int minutos, int segundos){
+	unsigned long seg;
 	
-	dias = 0;
-	/*Sumo los dias de los anios, sin tener en cuenta el actual */
-	dias = dias + (anio - 1) * 365;
-	/*Sumo los dias de los meses, sin tener en cuenta el actual*/
-	for(i = 1; i < mes; i++){
-		dias = dias + diasMes(i);
-	}
-	/*Sumo los dias*/
-	dias = dias + dia;
+	seg = 0;
+	/*Calculo la cantidad de segundos en la hora */
+	seg = seg + (hora * 60 * 60);
+	/*Calculo la cantidad de segundos en los minutos */
+	seg = seg + (minutos * 60);
+	/*Sumo los segundos*/
+	seg = seg + segundos;
 	
-	return dias;
+	return seg;
+}
+
+
+/*
+	Permite ingresar una hora entre el rango [0, 23]
+*/
+int ingresarHora(){
+	int hora;
+	
+	printf("Hora - ");
+	hora = ingresar(0, 23);
+	
+	return hora;
 }
 
 /*
-	Permite que el usuario ingrese una fecha. 
-	Formato dd/mm/aaaa.
+	Permite ingresar minutos entre el rango [0, 59]
 */
-
-void ingresarFecha(int *anio, int *mes, int *dia){
-	*anio = ingresarAnio();
-	*mes = ingresarMes();
-	*dia = ingresarDia(*mes);
+int ingresarMinutos(){
+	int minutos;
+	
+	printf("Minutos - ");
+	minutos = ingresar(0, 59);
+	
+	return minutos;
 }
 
 /*
-	Permite ingresar un anio entre el rango [1970, 2038]
+	Permite ingresar segundos entre el rango [0, 59].
 */
-int ingresarAnio(){
-	int anio;
+int ingresarSegundos(){
+	int segundos;
 	
-	printf("A%co - ", 164);
-	anio = ingresar(1970, 2038);
+	printf("Segundos - ");
+	segundos = ingresar(0, 59);
 	
-	return anio;
-}
-
-/*
-	Permite ingresar un mes entre el rango [1, 12]
-*/
-int ingresarMes(){
-	int mes;
-	
-	printf("Mes - ");
-	mes = ingresar(1, 12);
-	
-	return mes;
-}
-
-/*
-	Permite ingresar un dia valido de acuerdo al mes.
-*/
-int ingresarDia(int mes){
-	int dia;
-	
-	printf("Dia - ");
-	switch(mes){
-	case 1:case 3:case 5: case 7:
-	case 8:case 10:case 12:		
-		dia = ingresar(1, 31);
-		break;
-	case 4:case 6:case 9:case 11:	
-		dia = ingresar(1, 30);
-		break;
-	case 2:
-		dia = ingresar(1, 28);
-		break;
-	}
-	
-	return dia;
+	return segundos;
 }
 
 /*
